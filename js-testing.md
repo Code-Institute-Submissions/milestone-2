7789c4b4434f8ff6a76c4ea6d96692019c6cc7b9
James Horne <james99h39@gmail.com>
2018-09-11 08:38:49 +0000

    city search bar

---
+++ js-testing.md
@@ -0,0 +1,2 @@
+console.log('Hello')
+To test that the JavaScript file has loaded properly. I
---
+++ js/map.js
@@ -0,0 +1,43 @@
+// Initialise variables
+let category = $('input[type=radio][name=options]').get(0).value
+let city = null
+console.log(category)
+
+// Get autocomplete working
+var input = document.getElementById('cities')
+var options = {
+  types: ['(cities)'],
+  componentRestrictions: {country: 'uk'}
+}
+
+autocomplete = new google.maps.places.Autocomplete(input, options)
+
+
+// Does search on map
+const mapSearch = () => {
+    console.log('city', city)
+    console.log('category', category)
+    if (city == null) {
+        $('#city-error').removeClass('no')
+        return
+    } else {
+        $('#city-error').addClass('no')
+    }
+    
+    
+                                                            // todo - write google maps code to link to the data (category and city) I've just received
+}
+
+
+// Autocomplete callbacks
+autocomplete.addListener('place_changed', () => {
+    city = input.value
+    mapSearch()
+})
+
+
+// Radio button callbacks
+$('input[type=radio][name=options]').change(function() {
+    category=this.value
+    mapSearch()
+})
